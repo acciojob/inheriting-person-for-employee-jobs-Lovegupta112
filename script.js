a45e80c9ec55216b9921e377517a28c77ca12bcf
lovegupta112 <loveguptaofficials@gmail.com>
2023-06-21 14:35:33 +0530

    update code

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function Person(name, age) {
 Person.prototype.greet=function(){
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 }
-// const person1=new Person('rahul',20);
-// person1.greet();
+const person1=new Person('rahul',20);
+person1.greet();
 
 function Employee(name, age, jobTitle) {
     this.name=name;
